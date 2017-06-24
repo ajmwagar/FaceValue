3363695dc6c53a70a8e93e61934413b4519c31e6
Salamagundi <ajmw.subs@gmail.com>
2017-06-24 15:02:14 -0700

    Working Tracking

--- python/SquashR.py
+++ python/SquashR.py
@@ -5,7 +5,8 @@ import cv2
 
 # cascPath = sys.argv[1]
 # faceCascade = cv2.CascadeClassifier(cascPath)
-# faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('../data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('../data/haarcascades/haarcascade_eye.xml')
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -14,13 +15,14 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # faces = faceCascade.detectMultiScale(
-        # gray,
-        # scaleFactor=1.1,
-        # minNeighbors=5,
-        # minSize=(30, 30),
-        # flags=cv2.CASCADE_SCALE_IMAGE
-    # )
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5) 
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     # # Draw a rectangle around the faces
     # for (x, y, w, h) in faces:
