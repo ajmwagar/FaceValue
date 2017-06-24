1b4ccbd1c50a3304cafb0c1e6af55d665a8418c5
Salamagundi <ajmw.subs@gmail.com>
2017-06-24 09:40:28 -0700

    Merged files

--- python/SqaushR.py
+++
@@ -1,8 +0,0 @@
-""""SqaushR Core V 1.0"""
-import os
-import sklearn 
-from sklearn import datasets
-print("Hello SqaushR")
-iris = datasets.load_iris()
-
-print(iris.data)
--- python/SquashR.py
+++ python/SquashR.py
@@ -1,5 +1,13 @@
-""" SqaushR Core V 1.0 """
+""""SqaushR Core V 1.0"""
+import os
 from os.path import splitext
+import sklearn 
+from sklearn import datasets
+print("Hello SqaushR")
+iris = datasets.load_iris()
+print(iris.data)
+
+
 def splitext_(path):
     if len(path.split('.')) > 2:
         return path.split('.')[0], '.'.join(path.split('.')[-2:])
